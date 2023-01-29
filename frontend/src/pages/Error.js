ece13bb9729d0413c2145e165b81a75cdad8576b
Sukhrob <suhrob.m89@gmail.com>
2023-01-29 08:51:49 -0800

    Extracting error data and throwing responses

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,3 +1,28 @@
+import { useRouteError } from "react-router-dom";
+import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
+
 export default function ErrorPage() {
-  return <h1>An error occurred!</h1>;
+  const error = useRouteError();
+
+  let title = "An error occurred";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not Found";
+    message = "Could not find resource or page.";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
