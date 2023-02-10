46c13d93a5848bf78ed154e62535b5b268dc56d5
Suhrob <suhrob.m89@gmail.com>
2023-02-09 21:13:05 -0800

    Reusing action()s via request method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,9 @@ import EventDetailPage, {
 } from "./pages/EventDetail";
 import EventsRootLayout from "./pages/EventsRootLayout";
 import HomePage from "./pages/Home";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
+import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
+import { action as manipulateEventAction } from "./components/EventForm";
 
 function App() {
   const router = createBrowserRouter([
@@ -38,10 +39,18 @@ function App() {
                   element: <EventDetailPage />,
                   action: deleteEventAction,
                 },
-                { path: "edit", element: <EditEventPage /> },
+                {
+                  path: "edit",
+                  element: <EditEventPage />,
+                  action: manipulateEventAction,
+                },
               ],
             },
-            { path: "new", element: <NewEventPage />, action: newEventAction },
+            {
+              path: "new",
+              element: <NewEventPage />,
+              action: manipulateEventAction,
+            },
           ],
         },
       ],
