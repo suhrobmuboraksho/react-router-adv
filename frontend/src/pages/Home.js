abc4653b1fdc2073959181a14cf2f5944df57592
Suhrob <suhrob.m89@gmail.com>
2023-02-12 11:18:16 -0800

    Minor modification

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,10 @@
+import PageContent from "../components/PageContent";
 const HomePage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <PageContent title="Welcome">
+      <p>Browse all our amazing events!</p>
+    </PageContent>
+  );
 };
 
 export default HomePage;
