afc779b8db5b3734cc812dbc9faede52d15eeb6b
Suhrob <suhrob.m89@gmail.com>
2023-02-17 22:34:30 -0800

    Adding route protection

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,7 +1,17 @@
+import { redirect } from "react-router-dom";
+
 export function getAuthToken() {
   return localStorage.getItem("token");
 }
 
 export function tokenLoader() {
   return getAuthToken();
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+
+  if (!token) return redirect("/auth");
+
+  return null;
 }
